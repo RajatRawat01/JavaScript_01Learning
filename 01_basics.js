cb4e81b21ed8ccecdfd421eafffb8ad4a469aa81
rajat <rajatrawat117@gmail.com>
2021-03-14 13:58:08 +0530

    01_basics.js file with JS basics

---
+++ 01_basics.js
@@ -0,0 +1,29 @@
+console.log("Welcome to JS");
+
+console.log( 100+200 ); // 300
+
+console.log("100" + 100); //100100
+
+console.log(100 - "100");
+
+console.log( "10" * 10); //100
+
+console.log("10"/"10"); //1
+
+console.log(10 + "10"); //1010
+
+console.log(10+ +"10"); //20
+
+console.log(+"100" + +"100"); //200
+
+
+console.log(0.1 + 0,2); //0.3 
+                       //Ans is :  //0.300000 
+// JAvaScript interally follow 64 bit memory algorithm.
+// high accuracy algo.
+
+
+//undefined   Samba = bench(project)
+//null.   XXXXX (not recruited).
+
+
