d559d8ad0629333c6d92c24f3e1571eaad29697b
rajat <rajatrawat117@gmail.com>
2021-03-29 22:24:36 +0530

    Aysync and Sync Comm

--- promises.js
+++ promises.js
@@ -35,30 +35,30 @@
 //        console.log(posRes);
 //    },(errRes)=>{
 //        console.log(errRes);
-//    });
+// //    });
 
 
 
-let promise1 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("Promise1");
-    },0);
-});
+// let promise1 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("Promise1");
+//     },0);
+// });
 
-let promise2 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        //resolve("Promise2");
-      //**************************** */
-      //Out of 3, if one is failed, then other 2 will also not xecute.  
-        reject("Error");
-    },5000);
-});
+// let promise2 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         //resolve("Promise2");
+//       //**************************** */
+//       //Out of 3, if one is failed, then other 2 will also not xecute.  
+//         reject("Error");
+//     },5000);
+// });
 
-let promise3 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("Promise3");
-    },10000);
-});
+// let promise3 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("Promise3");
+//     },10000);
+// });
 
 // promise1.then((posRes)=>{
 //     console.log(posRes);
@@ -99,8 +99,122 @@ let promise3 = new Promise((resolve,reject)=>{
 
 
 //Which promise will run first, we can check using race
-Promise.race([promise1,promise2,promise3]).then((posRes)=>{
-    console.log(posRes);
-},(errorRes)=>{
-    console.log(errRes);
+// Promise.race([promise1,promise2,promise3]).then((posRes)=>{
+//     console.log(posRes);
+// },(errorRes)=>{
+//     console.log(errRes);
+// });
+
+
+
+//then() function use arrow function, as it causes trouble . We want to eradicate this.
+
+/* then we have concept , Async and await keyword
+//-----------------------------------------------
+
+  - we will consume promises in 2 ways.
+
+  1. then()
+  2. aync & await.
+
+  - then() function usage created the confusion beacause of callbacks.
+  - recommended to use async and await to consume promises.
+ - async & await keywords introduced in ES6.
+*/
+
+/*
+let promise1 = new Promise((resolve,reject)=>{
+
+     resolve("hello, welcometo async and await keyword");
 });
+
+//latest approach
+async function my_fun(){
+
+     const res = await promise1;
+     console.log(res);
+};
+
+my_fun();
+
+*/
+
+/*
+
+let promise1 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Promise....!");
+    },0);
+});
+
+
+let promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Promise....2!");
+    },5000);
+});
+
+
+let promise3 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Promise....3!");
+    },10000);
+});
+
+
+async function my_func()
+{
+    const data1 = await promise1;
+    const data2 = await promise2;
+    const data3 = await promise3;
+
+    console.log(data1,data2,data3);
+};
+
+my_func();
+
+*/
+
+//************************************************* */
+/* What is Synchronous communication and Asynchronous communication
+ 
+ - What is Synchronous Communication.
+   ------------------------------------
+    -Lets say there is a server and there are five clients(browsers).
+    - Fist client will submit request and after request gets success ful for 
+       1st client then only second client will only be able to send reqest and soo on,
+     - For eg. In movie Ticket , he checks each ticket before entering into theatre.
+
+    
+   - what is Asynchronous Communication.
+   ------------------------------------- 
+     No rules and regulation ..
+    - client 1, 2,3,4 entering into server at time parallery...
+    - client 1 request will not impact client2 request and so on.
+
+    for eg - movie finished, all will come out together parallrelly.
+ 
+
+    what is recommend ?
+     Always recommed - Asynchronous Communication.
+
+    - In Javascript...
+    ------------------
+    1. jQuery(library)
+     we use ajax() function
+     
+     In react...
+     ----------------
+     we use axios
+
+
+     => Async
+
+     => jQuery
+
+     => JSON Server
+
+     => GET
+
+     => POST
+*/
