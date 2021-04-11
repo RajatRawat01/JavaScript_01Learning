8cb51cd22db36bfdcf22384277edbf31a5ae15e9
rajat <rajatrawat117@gmail.com>
2021-04-11 14:38:27 +0530

    Slice() functions

--- arrays.js
+++ arrays.js
@@ -197,6 +197,70 @@ console.log(arr);
 
 arr.splice(0,0,10);
 console.log(arr);
-//Sourabh Gupta ---> Printer Repairing..
-9811215121
+
+
+
+*/
+//findIndex()
+//to know index of particular element
+
+/*
+    let arr = [10,20,30,40,50];
+    console.log(
+        arr.findIndex((element,index)=>{
+            return element === 30;
+        })
+    );     //2
+    console.log(
+        arr.findIndex((element,index)=>{
+            return element === 50;
+        })
+    );     //4
+    arr.splice(arr.findIndex((element,index)=>{
+        return element === 20;
+    }),1);
+    console.log(arr);    //[ 10, 30, 40, 50 ]
+    arr.splice(arr.findIndex((element,index)=>{
+        return element === 40;
+    }),1);
+    console.log(arr);    //[ 10, 30, 50 ]
+let arr = [{"p_id":111,"p_name":"p_one","p_cost":10000},
+            {"p_id":222,"p_name":"p_two","p_cost":20000},
+            {"p_id":333,"p_name":"p_three","p_cost":30000},
+            {"p_id":444,"p_name":"p_four","p_cost":40000},
+            {"p_id":555,"p_name":"p_five","p_cost":50000}];
+arr.splice(arr.findIndex((element,index)=>{
+    return element.p_id === 333;
+}),1);            
+console.log(arr);
+*/
+
+/*
+    //slice() - I need selected items....get the selectd items.
+    let arr = [10,20,30,40,50,60,70,80,90,100];          //immutable
+    console.log( arr.slice(4,7) );    //[ 50, 60, 70 ]
+    console.log( arr.slice(6,9) );     //[ 70, 80, 90 ]
+    console.log( arr.slice(1) );       //[20,30,40,50,60,70,80,90,100]
+
+    console.log([...arr.slice(7,10),...arr.slice(0,3)]);[80,90,100,10,20,30]
+
+  let arr2 = [0,1,2,3,4,5,6,7,8,9];
+
+  let a1 = arr2.slice(5);
+  let a2 = arr2.slice(0,5);
+  let a3 = a2.concat(a1);
+  console.log(a3);
+
+lt arr3 = [100,200,300,400,500,600,700,800,900,1000];
+
+console.log(arr3.slice(-6,-4) );
+
+want to print200,300,400
+console.log(arr3.slice(-9,-6)); //200,300,400
+console.log(arr3.slice(-10,-9)); //100
+    */
+
+// Interview Questions...
+  // What is diff between splice() and slic()
+
 
