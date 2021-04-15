ed528f03444adf2cd9f760533738fedecab0b797
rajat <rajatrawat117@gmail.com>
2021-04-15 14:09:28 +0530

    Currying Concept

---
+++ call.js
@@ -0,0 +1,38 @@
+//Can I access one memory location to another memory location...
+// ANS is NO.
+//If we want to set relationship between one memory loc to another memory loc
+/*
+    let obj = {
+        "num1" : 10
+    };
+    function my_fun(){
+        console.log( this.num1 + 10 );
+    };
+    my_fun.call(obj);    //20
+*/
+
+let obj = {
+    num1 : 10,
+    num2 : 20
+};
+
+function my_fun(arg1,arg2,arg3){
+    console.log( this.num1 + this.num2 + arg1 + arg2 + arg3 );
+};
+
+my_fun.call(obj,30,40,50);    //150
+
+my_fun.call(obj,30,40,50);    //150
+my_fun.call(obj,-10,-10,-10); //0
+
+let arr = [100,200,300];
+my_fun.apply(obj,arr);    //630
+
+let arr1 = [-10,-10,-10,-10];
+my_fun.apply(obj,arr1);   //0
+
+let final_fun = my_fun.bind(obj);
+final_fun(10,10,10);   //60
+
+//merging function memory n jdon memory using bind and newmemory oc will be created
+my_fun.bind(obj)(-10,-10,-10);   //0
